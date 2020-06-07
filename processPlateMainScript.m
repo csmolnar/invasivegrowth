b3fc4cd906909721770bcdc1fb8375ed02bdb47e
Csaba Molnar <molnar.csaba@brc.mta.hu>
2020-06-07 18:02:39 -0400

    Refactor processing steps.

--- processPlateMainScript.m
+++ processPlateMainScript.m
@@ -7,17 +7,23 @@ global options;
 
 setOptions(scriptDir);
 
+%%
 beforeImagesList = dir(fullfile(options.dataDir, options.beforeRegexp));
 afterImagesList = dir(fullfile(options.dataDir, options.afterRegexp));
 
 registeredImages = cell(length(afterImagesList),2);
 masks = cell(length(afterImagesList),1);
 
-% register images to each other
-fprintf('Registration of images started.\n');
+mbs = cell(length(afterImagesList));
+mas = cell(length(afterImagesList));
+
+%%
+fprintf(sprintf('Processing of images started.\n'));
 for i=1:length(afterImagesList)
     
-    fprintf('[%02d/%02d] Registering image %s\n', i, length(afterImagesList), beforeImagesList(i).name);
+    fprintf('[%02d/%02d] Processing image pair (%s,%s)\n', i, length(afterImagesList), beforeImagesList(i).name, afterImagesList(i).name);
+    
+    fprintf('|-[1] Registration of images\n');
     
     % rotation with 180 degrees
     imgBefore = imrotate(imread( fullfile(options.dataDir, beforeImagesList(i).name) ), 180 );
@@ -26,26 +32,7 @@ for i=1:length(afterImagesList)
     % image registration registration
     [registeredImages{i,1}, registeredImages{i,2}, masks{i}] = registerImages(imgBefore, imgAfter);
 
-    if options.saveSegmentations == 1
-        f = figure('Visible','off');
-        h = imshowpair(registeredImages{i,1}, registeredImages{i,2}, 'Scaling', 'joint');
-        imwrite(getimage(h), fullfile(options.resultsDir, ['registration_' beforeImagesList(i).name '.png']));
-    end
-    
-end
-fprintf('Registration finished.\n');
-
-% save(fullfile(options., sprintf('tempVars_%s.mat', options.plateName)));
-
-%%
-fprintf(sprintf('Processing of images.'));
-% referenceImageMask = imread(fullfile(options.dataDir, options.referenceMaskPath)) > 0;
-
-mbs = cell(length(afterImagesList));
-mas = cell(length(afterImagesList));
-
-for i=1:length(afterImagesList)
-    fprintf('[%02d/%02d] Processing image pair (%s,%s)\n', i, length(afterImagesList), beforeImagesList(i).name, afterImagesList(i).name);
+    fprintf('|-[2] Segmentation and feature extraction\n');
     
     if strcmp(options.imagingType, 'top')
         [mbs{i}, mas{i}, f, plateMask, segm] = processImagePairTop(registeredImages{i,1}, registeredImages{i,2}, masks{i});
@@ -53,7 +40,14 @@ for i=1:length(afterImagesList)
         [mbs{i}, mas{i}, f, plateMask, segm] = processImagePairBottom(registeredImages{i,1}, registeredImages{i,2}, masks{i});
     end
     
+    fprintf('|-[3] Saving image results\n');
+    
     if options.saveSegmentations == 1
+        
+        h = figure('Visible','off');
+        imshowpair(registeredImages{i,1}, registeredImages{i,2}, 'Scaling', 'joint');
+        imwrite(getimage(h), fullfile(options.resultsDir, ['registration_' beforeImagesList(i).name '.png']));
+        
 %         imwrite(plateMask, fullfile(options.resultsDir, sprintf('mask_%s', beforeImagesList(i).name)));
         imwrite(segm, fullfile(options.resultsDir, sprintf('segm_%s', beforeImagesList(i).name)));
         imwrite(getimage(f), fullfile(options.resultsDir, sprintf('cont_%s', beforeImagesList(i).name)));
@@ -61,6 +55,7 @@ for i=1:length(afterImagesList)
     
 end
 fprintf('Processing finished.\n');
+
 
 %%
 fprintf('Saving measurements to table...\n');
